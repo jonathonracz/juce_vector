f19879d9ab468f46a1961419b4c483fc7ed68439
Antonio Lassandro <lassandroan@gmail.com>
2018-02-18 00:15:27 -0600

    Adding SVG Doctype Definition

--- juce_vector.h
+++ juce_vector.h
@@ -38,6 +38,10 @@
 
 #pragma once
 
+
+#define SVGDOCTYPE "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">"
+
+
 #include <juce_graphics/juce_graphics.h>
 
 #include "context/LowLevelGraphicsSVGRenderer.h"
